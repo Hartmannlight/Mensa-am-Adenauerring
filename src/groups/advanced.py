033e3b5c1f6ca10e23ad5120e51f02ffaaa9c1e8
Nathaniel Hartmann <hartmannlight@gmail.com>
2023-10-15 00:09:27 +0200

    logging, some refactoring

--- src/groups/advanced.py
+++ src/groups/advanced.py
@@ -1,32 +1,46 @@
-import plan
-import time
+from src import plan
+import logging
 
+import time
 import os
 import git
 
 import discord
 from discord import app_commands
 
+logger = logging.getLogger("bot")
+
 
 class Advanced(app_commands.Group):
     @app_commands.command()
     async def version(self, interaction: discord.Interaction):
+        logger.info(f"{interaction.user} requested the bot version - {interaction.data['options']} "
+                    f"({interaction.user.id} in {interaction.guild.id}.{interaction.channel.id})")
+
         repo = git.Repo(os.getcwd(), search_parent_directories=True)
         sha = repo.head.object.hexsha
         await interaction.response.send_message(sha)  # noqa
 
     @app_commands.command()
     async def update(self, interaction: discord.Interaction):
-        print(f'{interaction.user} requested an update')
+        logger.info(f"{interaction.user} requested a plan update - {interaction.data['options']} "
+                    f"({interaction.user.id} in {interaction.guild.id}.{interaction.channel.id})")
+
         await interaction.response.defer(ephemeral=True, thinking=True)  # noqa
         await plan.update_plan()
         await interaction.followup.send(f"Speiseplan aktualisiert.", ephemeral=False)
 
     @app_commands.command()
     async def home(self, interaction: discord.Interaction):
+        logger.info(f"{interaction.user} requested the source code - {interaction.data['options']} "
+                    f"({interaction.user.id} in {interaction.guild.id}.{interaction.channel.id})")
+
         await interaction.response.send_message("https://github.com/Hartmannlight/Mensa-am-Adenauerring")  # noqa
 
     @app_commands.command()
     async def ping(self, interaction: discord.Interaction):
+        logger.info(f"{interaction.user} requested the ping - {interaction.data['options']} "
+                    f"({interaction.user.id} in {interaction.guild.id}.{interaction.channel.id})")
+
         latency = interaction.created_at.timestamp() - time.time()
         await interaction.response.send_message(f"Pong! ({latency * 1000:.0f}ms)")  # noqa
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 
 import config
-import plan
+from src import plan
+
+import logging
 
 import datetime
 import holidays
@@ -15,43 +17,40 @@ from groups.mensa import Mensa
 
 bot = discord.Client(intents=discord.Intents.default())
 tree = discord.app_commands.CommandTree(bot)
+logger = logging.getLogger("bot")
 
 
 @bot.event
 async def on_ready():
-
-    print("Logged in as", bot.user.name, bot.user.id)
-
+    logger.info(f"{bot.user} connected to Discord")
     tree.add_command(Mensa(name="mensa", description="Zeigt den aktuellen Speiseplan der Mensa an."))
     tree.add_command(Advanced(name="advanced", description="Entwickler-Optionen"))
     tree.copy_global_to(guild=discord.Object(id=config.GUILD))
     await tree.sync(guild=discord.Object(id=config.GUILD))
-    print("synced commands")
+    logger.debug("Command tree has been synced")
 
     daily_post.start()
     update_plan.start()
-    print("started tasks")
 
 
 @tasks.loop(time=datetime.time(hour=9, minute=30, tzinfo=ZoneInfo("Europe/Berlin")))
 async def daily_post():
-    print("Daily post started")
+    logger.debug("Preparing daily post")
     today = datetime.date.today()
     public_holidays = holidays.country_holidays("DE", subdiv="BW")
     if today.weekday() >= 5 or public_holidays.get(today):
+        logger.info(f"Skipping daily post on {today} because it's a public holiday or the weekend")
         return
 
     embed = plan.get_embed(today)
     await bot.get_channel(config.CHANNEL_ID).send(embed=embed)
-    print("Daily post finished")
+    logger.info(f"Daily post on {today} has been sent")
 
 
 @tasks.loop(seconds=config.UPDATE_INTERVAL)
 async def update_plan():
-    print("Auto update started")
     await plan.update_plan()
-    print("Auto update finished")
 
 
 if __name__ == "__main__":
-    bot.run(config.BOT_TOKEN)
+    bot.run(token=config.BOT_TOKEN, root_logger=True)
